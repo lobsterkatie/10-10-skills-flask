0955aad0f584d1ea6837fb4e832d3f09419e0c79
Katie Byers <lobsterkatie@gmail.com>
2015-10-10 23:20:30 -0700

    whole bunch of changes made; forgot to commit them until now - whoops.

--- application.py
+++ application.py
@@ -2,18 +2,30 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+#####---------- app routes ----------#####
 
 @app.route("/")
 def index_page():
     """Show an index page."""
 
-    return "<html><body>This is the homepage.</body></html>"
+    return render_template("index.html")
 
-    # Alternately, we could make this a Jinja template in `templates/`
-    # and return that result of rendering this, like:
-    #
-    # return render_template("index.html")
 
+@app.route("/spurned")
+def rejected_page():
+    """React to the cruel rejection"""
+
+    return render_template("rejection.html")
+
+
+@app.route("/application-form")
+def application_page():
+    """Show the application form"""
+
+    return render_template("application-form.html")
+
+
+#####---------- code to run the server below ----------#####
 
 if __name__ == "__main__":
     app.run(debug=True)
